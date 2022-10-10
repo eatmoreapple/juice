5851918ceb98313bb24de666cb26a227e7fe59f0
eatmoreapple <eatmoreorange@gmail.com>
2022-10-10 23:40:50 +0800

    update the way of get id from interface

--- juice.go
+++ juice.go
@@ -11,10 +11,20 @@ import (
 
 // Engine is the main struct of pillow
 type Engine struct {
+
+	// Configuration is the configuration of the engine
+	// It is used to initialize the engine and to get the mapper statements
 	Configuration *Configuration
-	Driver        driver.Driver
-	DB            *sql.DB
-	Logger        *log.Logger
+
+	// Driver is the driver used by the engine
+	// It is used to initialize the database connection and translate the mapper statements
+	Driver driver.Driver
+
+	// DB is the database connection
+	DB *sql.DB
+
+	// Logger is the logger used by the engine
+	Logger *log.Logger
 }
 
 // Statement implements the Statement interface
@@ -29,17 +39,23 @@ func (e *Engine) Tx() TxMapperExecutor {
 	return &txStatement{engine: e, tx: tx, err: err}
 }
 
+// init initializes the engine
 func (e *Engine) init() error {
+
+	// get the default environment from the configuration
 	env, err := e.Configuration.Environments.DefaultEnv()
 	if err != nil {
 		return err
 	}
+
+	// try to get the driver from the configuration
 	drv, err := driver.Get(env.Driver)
 	if err != nil {
 		return err
 	}
 	e.Driver = drv
 
+	// open the database connection
 	e.DB, err = env.Connect()
 	if err != nil {
 		return err
@@ -47,13 +63,22 @@ func (e *Engine) init() error {
 	return nil
 }
 
-func (e *Engine) getMapperStatement(v any) (Statement, error) {
-	id, err := FuncForPC(v)
-	if err != nil {
-		return nil, err
+// try to get the statement from the configuration with the given interface
+func (e *Engine) getMapperStatement(v any) (stat Statement, err error) {
+	var id string
+
+	// if the interface is a string, use it as the id
+	if str, ok := v.(string); ok {
+		id = str
+	} else {
+		// else try to get the id from the interface
+		if id, err = FuncForPC(v); err != nil {
+			return nil, err
+		}
 	}
 
-	stat, err := e.Configuration.Mappers.GetStatementByID(id)
+	// try to get the statement from the configuration
+	stat, err = e.Configuration.Mappers.GetStatementByID(id)
 
 	if err != nil {
 		return nil, fmt.Errorf("mapper %s not found", id)
