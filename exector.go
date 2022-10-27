0c337f8d7195c43391c5dd97843d4b533e68e0f3
eatmoreapple <eatmoreorange@gmail.com>
2022-10-27 14:29:43 +0800

    set session into context

--- exector.go
+++ exector.go
@@ -40,6 +40,7 @@ func (e *executor) QueryContext(ctx context.Context, param interface{}) (*sql.Ro
 	}
 	middlewares := e.engine.middlewares
 	stmt := e.statement
+	ctx = WithSession(ctx, e.session)
 	return middlewares.QueryContext(stmt, e.session.QueryContext)(ctx, query, args...)
 }
 
@@ -56,6 +57,7 @@ func (e *executor) ExecContext(ctx context.Context, param interface{}) (sql.Resu
 	}
 	middlewares := e.engine.middlewares
 	stmt := e.statement
+	ctx = WithSession(ctx, e.session)
 	return middlewares.ExecContext(stmt, e.session.ExecContext)(ctx, query, args...)
 }
 
