6740db0f794fc41396aaf855c728e7cb10b08671
eatmoreapple <eatmoreorange@gmail.com>
2023-01-01 12:57:19 +0800

    fix code gen

--- cmd/juice/testcase/interface.go
+++ cmd/juice/testcase/interface.go
@@ -9,9 +9,9 @@ import (
 //go:generate juice --type Interface --config config.xml --namespace main.UserRepository --output interface_impl.go
 type Interface interface {
 	// GetUserByID 根据用户id查找用户
-	GetUserByID(ctx context.Context, id int64) (*User, error)
+	GetUserByID(ctx context.Context, id int64) ([]*user.User, error)
 	// CreateUser 创建用户
-	CreateUser(ctx context.Context, u *user.User) error
+	CreateUser(ctx context.Context, u map[string]*user.User) error
 	// DeleteUserByID 根据id删除用户
 	DeleteUserByID(ctx context.Context, id int64) (sql.Result, error)
 }
