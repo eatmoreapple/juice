4a21b630a36c2bb3403feadf9dff08ea1f6cf5cb
eatmoreapple <eatmoreorange@gmail.com>
2023-05-22 15:03:06 +0800

    style: refine session

--- session.go
+++ session.go
@@ -7,24 +7,7 @@ import (
 
 // Session is a wrapper of sql.DB and sql.Tx
 type Session interface {
-	Query(query string, args ...any) (*sql.Rows, error)
 	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
-	Exec(query string, args ...any) (sql.Result, error)
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
-	Prepare(query string) (*sql.Stmt, error)
 	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
-}
-
-// sessionKey is the key of the session in the context.
-type sessionKey struct{}
-
-// SessionWithContext returns a new context with the session.
-func SessionWithContext(ctx context.Context, session Session) context.Context {
-	return context.WithValue(ctx, sessionKey{}, session)
-}
-
-// SessionFromContext returns the session from the context.
-func SessionFromContext(ctx context.Context) Session {
-	session, _ := ctx.Value(sessionKey{}).(Session)
-	return session
 }
