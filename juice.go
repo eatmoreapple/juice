cef1eb692ace1fd7b7224ca51de93274ded223a2
多吃点苹果 <eatmoreorange@gmail.com>
2023-06-27 20:36:59 +0800

    update executor create (#171)

--- juice.go
+++ juice.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 eatmoreapple
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package juice
 
 import (
@@ -37,12 +53,13 @@ type Engine struct {
 	cacheFactory func() cache.Cache
 }
 
+// executor represents a mapper executor with the given parameters
 func (e *Engine) executor(v any) (*executor, error) {
 	stat, err := e.GetConfiguration().Mappers.GetStatement(v)
 	if err != nil {
 		return nil, err
 	}
-	return &executor{statement: stat}, nil
+	return &executor{statement: stat, session: e.DB()}, nil
 }
 
 // Object implements the Manager interface
@@ -51,7 +68,6 @@ func (e *Engine) Object(v any) Executor {
 	if err != nil {
 		return inValidExecutor(err)
 	}
-	exe.session = e.DB()
 	return exe
 }
 
