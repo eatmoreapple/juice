4a9aad11a19d2230f917f63a5b3e95c1587cb689
eatmoreapple <eatmoreorange@gmail.com>
2024-10-29 18:09:13 +0800

    feat: update juicecli code generate

--- juicecli/impl/testdata/interface.go
+++ juicecli/impl/testdata/interface.go
@@ -9,6 +9,8 @@ import (
 type Interface interface {
 	// GetUserByID 根据用户id查找用户
 	GetUserByID(ctx context.Context, id int64) (User, error)
+	// GetUserByIDs 根据用户id查找用户
+	GetUserByIDs(ctx context.Context, ids []int64) (User, error)
 	// CreateUser 创建用户
 	CreateUser(ctx context.Context, u map[string]*User) error
 	// DeleteUserByID 根据id删除用户
