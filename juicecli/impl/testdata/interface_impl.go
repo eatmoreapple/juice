0dda2e698dc67867fc816f7a05d5eb219defec34
多吃点苹果 <eatmoreorange@gmail.com>
2024-10-29 18:35:31 +0800

    fix: point built type distinguish (#330)

--- juicecli/impl/testdata/interface_impl.go
+++ juicecli/impl/testdata/interface_impl.go
@@ -10,7 +10,7 @@ import (
 
 type InterfaceImpl struct{}
 
-func (i InterfaceImpl) GetUserByID(ctx context.Context, id int64) (result0 User, result1 error) {
+func (i InterfaceImpl) GetUserByID(ctx context.Context, id *int64) (result0 User, result1 error) {
 	manager := juice.ManagerFromContext(ctx)
 	var iface Interface = i
 	executor := juice.NewGenericManager[User](manager).Object(iface.GetUserByID)
