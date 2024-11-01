263cd2bd9765571625469b2c4203ee0b864e9390
多吃点苹果 <eatmoreorange@gmail.com>
2024-11-01 11:16:08 +0800

    rename stdpath to unixpath (#335)

--- fs.go
+++ fs.go
@@ -1,9 +1,25 @@
+/*
+Copyright 2023 eatmoreapple
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package juice
 
 import (
 	"io/fs"
 	"os"
-	stdpath "path"
+	unixpath "path"
 	"path/filepath"
 )
 
@@ -23,7 +39,10 @@ type fsWrapper struct {
 	baseDir string
 }
 
+// Open opens the named file within the base directory of the fsWrapper.
+// It joins the base directory and the name using Unix-style path separators,
+// ensuring compatibility with io/fs.Open which uses slash-separated paths on all systems.
 func (f fsWrapper) Open(name string) (fs.File, error) {
-	path := stdpath.Join(f.baseDir, name)
+	path := unixpath.Join(f.baseDir, name)
 	return f.fs.Open(path)
 }
